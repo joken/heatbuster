1734a4c4151a3ea1827d268ee4b06cccd03d0214
kyumina <kyumina.x.sighn@gmail.com>
2016-07-23 17:27:42 +0900

    ようやくBLEApiを叩き始めたよ。テスト用に別のprojectを作ったよ。現時点ではBLEに対応しているかの判定しかないよ。

---
+++ BLEtest/app/src/main/java/io/github/joken/bletest/MainActivity.java
@@ -0,0 +1,22 @@
+package io.github.joken.bletest;
+
+import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Use this check to determine whether BLE is supported on the device. Then
+        // you can selectively disable BLE-related features.
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+}
