e785dd2e9788f50c8b36a9289fd1e0c374a78f50
mi-24v <yuusukessid@gmail.com>
2016-08-01 08:37:52 +0900

    created login task(beta version)

--- Heatbuster/app/src/main/java/io/github/joken/heatbuster/LoginActivity.java
+++ Heatbuster/app/src/main/java/io/github/joken/heatbuster/LoginActivity.java
@@ -33,8 +33,13 @@ import android.widget.TextView;
 
 import com.beardedhen.androidbootstrap.BootstrapButton;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
@@ -50,11 +55,10 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
 
     /**
      * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
      */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
+//    private static final String[] DUMMY_CREDENTIALS = new String[]{
+//            "foo@example.com:hello", "bar@example.com:world"
+//    };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
@@ -298,37 +302,46 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
      * Represents an asynchronous login/registration task used to authenticate
      * the user.
      */
-    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+    class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
 
         private final String mEmail;
         private final String mPassword;
+		private final OkHttpClient mclient;
+		private String token;
+
+		//TODO LoginAPIの決定
+		private static final String loginApi = "http://heatbuster/login";
 
         UserLoginTask(String email, String password) {
             mEmail = email;
             mPassword = password;
+			mclient = new OkHttpClient();
         }
 
         @Override
         protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
+			String query = loginApi + mEmail + mPassword;
+			Request request = new Request.Builder().url(query).build();
+			Response response;
+			String result = null;
 
             try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
+				response = mclient.newCall(request).execute();
+				result = response.body().string();
+            } catch (Exception e) {
+				e.printStackTrace();
                 return false;
             }
 
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
+//            for (String credential : DUMMY_CREDENTIALS) {
+//                String[] pieces = credential.split(":");
+//                if (pieces[0].equals(mEmail)) {
+//                    // Account exists, return true if the password matches.
+//                    return pieces[1].equals(mPassword);
+//                }
+//            }
 
-            // TODO: register the new account here.
-            return true;
+            return parseResult(result);
         }
 
         @Override
@@ -338,6 +351,7 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
 
             if (success) {
                 Intent intent = new Intent(getApplication(), MainActivity.class);
+				intent.putExtra("token", token);
                 startActivity(intent);
                 finish();
             } else {
@@ -351,6 +365,18 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
             mAuthTask = null;
             showProgress(false);
         }
+
+		/**
+		 * 認証成否を判定する
+		 * @param query サーバーからのレスポンス
+		 * @return 認証成功ならtrue、そうでないならfalse
+		 * */
+		private boolean parseResult(String query){
+			//TODO ロジックの実装
+			//TODO tokenのセット(成功時)
+			token = null;
+			return true;
+		}
     }
 }
 
