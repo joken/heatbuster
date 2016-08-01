b271c729e3ea8511c12baa119f0b629307d9f44a
kyumina <kyumina.x.sighn@gmail.com>
2016-08-01 15:14:53 +0900

    フェーズデバイスのクリエイティブなスキャンが出来るようになる――つまり、次の段階へとステップアップしたよ。Androidスケール以降のBLEScanではポジショニングインターナショナル•データベースをリスペクトするベネフィットそれように書きなおしたよ。

--- BLEtest/app/src/main/java/io/github/joken/bletest/MainActivity.java
+++ BLEtest/app/src/main/java/io/github/joken/bletest/MainActivity.java
@@ -1,11 +1,36 @@
 package io.github.joken.bletest;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Handler;
+import android.os.ParcelUuid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    // 10秒後にスキャンを停止
+    private static final long SCAN_PERIOD = 10000;
+
+    private final static int REQUEST_PERMISSIONS = 1;
+    private final static int SDKVER_MARSHMALLOW = 23;
+
+    private Handler mHandler = new Handler();
+    private BluetoothAdapter mBluetoothAdapter;
+    boolean mScanning=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +43,102 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
             finish();
         }
+
+        // Android6.0以降なら権限確認.
+        if(Build.VERSION.SDK_INT >= SDKVER_MARSHMALLOW)
+        {
+            this.requestBlePermission();
+        }
+
+        // Initializes Bluetooth adapter.
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        // Ensures Bluetooth is available on the device and it is enabled. If not,
+        // displays a dialog requesting user permission to enable Bluetooth.
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
     }
+    @TargetApi(SDKVER_MARSHMALLOW)
+    private void requestBlePermission(){
+        // 権限が許可されていない場合はリクエスト.
+        if(checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION
+            },REQUEST_PERMISSIONS);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        // 権限リクエストの結果を取得する.
+        if (requestCode == REQUEST_PERMISSIONS) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(MainActivity.this, "Succeed", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+            }
+        }else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    public void onClickScan(View v) {
+        // 10秒後にBLE機器のスキャンを開始します
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mBluetoothAdapter.startLeScan(mLeScanCallback);
+            }
+        }, SCAN_PERIOD);
+    }
+
+    /**
+     * スキャン停止ボタンタップ時のコールバックメソッド
+     * @param v
+     */
+    public void onClickStop(View v) {
+        // BLE機器のスキャンを停止します
+        mBluetoothAdapter.stopLeScan(mLeScanCallback);
+    }
+
+    /*
+    private void scanLeDevice(final boolean enable){
+        if (enable) {
+            //Stops scanning after a pre-defined scan period.
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mScanning = false;
+                    mBluetoothAdapter.stopLeScan(mLeScanCallback);
+                }
+            }, SCAN_PERIOD);
+            mScanning = true;
+            mBluetoothAdapter.startLeScan(mLeScanCallback);
+        } else {
+            mScanning = false;
+            mBluetoothAdapter.stopLeScan(mLeScanCallback);
+        }
+    }*/
+
+    private BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback(){
+        @Override
+        public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
+            // スキャンできた端末の情報をログ出力
+            ParcelUuid[] uuids = device.getUuids();
+            String uuid = "";
+            if (uuids != null) {
+                for (ParcelUuid puuid : uuids) {
+                    uuid += puuid.toString() + " ";
+                }
+            }
+            String msg = "name=" + device.getName() + ", bondStatus="
+                    + device.getBondState() + ", address="
+                    + device.getAddress() + ", type" + device.getType()
+                    + ", uuids=" + uuid;
+            Log.d("BLEActivity", msg);
+
+        }
+    };
 }
