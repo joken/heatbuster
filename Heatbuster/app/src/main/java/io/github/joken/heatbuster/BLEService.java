b289e5f8671d903b7f59c76ff0b5915ca86c4565
mi-24 <miwpayou0808@gmail.com>
2016-09-02 18:28:19 +0900

    [UPDATED] BLEServiceをちょっとだけすすめた

--- Heatbuster/app/src/main/java/io/github/joken/heatbuster/BLEService.java
+++ Heatbuster/app/src/main/java/io/github/joken/heatbuster/BLEService.java
@@ -1,16 +1,36 @@
 package io.github.joken.heatbuster;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Messenger;
 
 public class BLEService extends Service {
+	private Messenger mMessenger;
+
 	public BLEService() {
+	}
+
+	@Override
+	public void onCreate(){
+		super.onCreate();
+		mMessenger = new Messenger(new MessageHandler(this.getApplicationContext()));
 	}
 
 	@Override
 	public IBinder onBind(Intent intent) {
 		// TODO: Return the communication channel to the service.
 		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	static  class MessageHandler extends Handler{
+		private Context mContext;
+
+		public MessageHandler(Context ct){
+			this.mContext = ct;
+		}
+
 	}
 }
