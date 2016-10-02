3f9547a2cf70e70a7817b76eb5fe7bce9c14ae68
mi-24v <miwpayou0808@gmail.com>
2016-10-02 11:17:33 +0900

    [UPDATED]<BLEService::Service>クライアントとの通信部、GattCallBackの実装(途中まで)

--- Heatbuster/app/src/main/java/io/github/joken/heatbuster/BLEService.java
+++ Heatbuster/app/src/main/java/io/github/joken/heatbuster/BLEService.java
@@ -2,6 +2,10 @@ package io.github.joken.heatbuster;
 
 import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.content.Intent;
@@ -10,21 +14,31 @@ import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
 
-import static android.support.v4.app.ActivityCompat.startActivityForResult;
+import java.util.ArrayList;
 
 public class BLEService extends Service {
-	public static final String REQUEST_BLUETOOTH = "request_bluetooth";
+	/** Key-Value方式のKeyあるいはRequest ID */
+	public static final String REQUEST_BLUETOOTH = "request_bluetooth";//BTの有効化リクエストキー
+	public static final String REQUEST_BLUETOOTH_ACTION = "request_bluetooth_action";//BTの有効化リクエストアクションToken
+	public static final String BLE_DEVICE = "connection_devices";//接続対象デバイスリストのキー
+	public static final String CLUB_INDEX = "club_index";//BT端末追加時の部活動指定キー
+	public static final int BLE_ADD_DEVICE = 931;//BT端末追加のMessageID
+	public static final int TOKEN_ADD = 514114;//Token追加のMessageID
 
-	private Messenger mMessenger;
-	private String token;
+	private Messenger mMessenger;//メッセンジャー
+	private static String token;//LoginToken
+	private static ArrayList<Clubmonitor> clubList;//監視リスト
 	private BluetoothAdapter mBluetoothAdapter;
+	private BluetoothGattCallback mGattCallback;
 
 	public BLEService() {
+		initGattCallBack();
 	}
 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId){
 		token = intent.getStringExtra("token");//Tokenをセット
+		clubList = new ArrayList<>();
 
 		// Initializes Bluetooth adapter.
 		final BluetoothManager bluetoothManager =
@@ -37,12 +51,35 @@ public class BLEService extends Service {
 			Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
 			//MainActivityにBluetooth有効化を促す
 			Intent startIntent = new Intent();
-			startIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			startIntent.setAction(REQUEST_BLUETOOTH_ACTION);
 			startIntent.putExtra(REQUEST_BLUETOOTH, enableBTIntent);
-			startActivity(enableBTIntent);
+			sendBroadcast(startIntent);
 		}
 
+		startBLEGatt();
+
 		return START_STICKY;
+	}
+
+	private void initGattCallBack(){
+		mGattCallback = new BluetoothGattCallback() {
+			@Override
+			public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+				super.onConnectionStateChange(gatt, status, newState);
+			}
+
+			@Override
+			public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+				if(status == BluetoothGatt.GATT_SUCCESS){
+					byte[] raw_data = characteristic.getValue();
+					//TODO パースして結果をなにかに格納する
+				}
+			}
+		};
+	}
+
+	private void startBLEGatt(){
+		//TODO 別スレッドで監視する
 	}
 
 	@Override
@@ -64,7 +101,18 @@ public class BLEService extends Service {
 		}
 
 		@Override
-		public void handleMessage(Message msg){}
+		@SuppressWarnings("unchecked")
+		public void handleMessage(Message msg){
+			switch (msg.what){
+				case BLE_ADD_DEVICE:
+					clubList.get(msg.getData().getInt(CLUB_INDEX)).setDeviceList((ArrayList<CheckBoxItem>)msg.getData().getSerializable(BLE_DEVICE));
+					break;
+				case TOKEN_ADD:
+					token = (String)msg.obj;
+					break;
+			}
+		}
 
 	}
+
 }
